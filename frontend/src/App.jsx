4d2b01860c21b16e3903378ffce7a05eba01310c
Sudhanshu Ranjan <sudhanshuranjan3107@gmail.com>
2024-08-05 15:59:27 +0530

    add tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from './Card'
 
 function App() {
 
   return (
-   <h1>Hellow</h1>
+   <>
+   <Card />
+   </>
   )
 }
 
